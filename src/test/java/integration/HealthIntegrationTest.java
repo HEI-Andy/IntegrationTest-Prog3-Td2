91c4cfd2ed9476e9b13bf9cb1857de035cd8bc0c
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-02-03 13:00:38 +0300

    Update

--- src/test/java/integration/HealthIntegrationTest.java
+++ src/test/java/integration/HealthIntegrationTest.java
@@ -6,17 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-
-import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = FootApi.class)
 @AutoConfigureMockMvc
@@ -25,19 +19,13 @@ public class HealthIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
-    public void pong() throws Exception {
-        RequestBuilder builder = get("/ping");
+    void pong() throws Exception{
         MockHttpServletResponse response = mockMvc
-                .perform(builder)
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(new MediaType(MediaType.TEXT_PLAIN, StandardCharsets.UTF_8)))
+                .perform(get("/ping"))
                 .andReturn()
                 .getResponse();
 
-        String expected = "pong";
-        String actual = response.getContentAsString();
-
-        assertEquals(response.getStatus(), HttpStatus.OK.value());
-        assertEquals(expected, actual);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("pong", response.getContentAsString());
     }
 }
